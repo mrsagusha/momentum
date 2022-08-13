378095a1977ff182cea14591de54f6dc0b47fc11
Alexandr <golubovskiy.al@mail.ru>
2022-08-13 16:41:34 +0300

    fix: fix smooth loading of images

--- src/setBackground.ts
+++ src/setBackground.ts
@@ -1,22 +1,14 @@
+/* eslint-disable prefer-template */
 import setTimeOfTheDay from './setTimeOfTheDay';
 
 const setBackground = (numberOfImage: string): void => {
   const timeOfDay: string = setTimeOfTheDay();
   const body = document.querySelector('body');
-
-  body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${numberOfImage}.jpg')`;
+  const img = new Image();
+  img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${numberOfImage}.jpg`;
+  img.addEventListener('load', (): void => {
+    body.style.backgroundImage = `url(${img.src})`;
+  });
 };
 
 export default setBackground;
-
-// когда будет норм интернет поменять кусок кода
-/* const setBackground = (numberOfImage: string): void => {
-  const timeOfDay: string = setTimeOfTheDay();
-  const body = document.querySelector('body');
-  const img = new Image();
-  img.src = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${numberOfImage}.jpg')`;
-  img.addEventListener('load', (): void => {
-    console.log(img);
-    body.style.backgroundImage = img.src;
-  });
-}; */
